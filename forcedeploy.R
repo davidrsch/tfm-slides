e7f30a6e136636ac2973ff799856c5502b2419d1
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-04-08 12:17:22 +0000

    Built site for gh-pages

--- forcedeploy.R
+++
@@ -1,4 +0,0 @@
-# This script is added to force deployment
-# for some reaseon quarto never isnt deploying
-# after executing renderlangvers.R
-1+1
--- renderlangvers.R
+++
@@ -1,2 +0,0 @@
-quarto::quarto_render("eng/index.qmd", as_job = F)
-quarto::quarto_render("gale/index.qmd", as_job = F)
